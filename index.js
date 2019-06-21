879971b8dfdb1f11f0df0a95791c78c65b78169e
nixace-chetan <52037283+nixace-chetan@users.noreply.github.com>
2019-06-21 17:56:57 +0530

    Port change

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World V1\n');
-}).listen(8081);
+   response.end('Hello World V3\n');
+}).listen(80);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:80/');
